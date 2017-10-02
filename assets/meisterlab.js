5a80c91557c0291f1109fb03aa8887007e39724c
meisterlab <32436405+meisterlab@users.noreply.github.com>
2017-10-02 22:42:37 +0600

    Update meisterlab.js

--- assets/meisterlab.js
+++ assets/meisterlab.js
@@ -1,15 +1,15 @@
 <script>
-function get(url,receiver) {
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-document.getElementById(receiver).href= this.responseText;
-return false;
-}
-};
-xhr.open ("GET" ,url, true);
-xhr.send ();
-return false;
-}
-get("http://meisterlab.github.io/assets/current.md","meisterlab");
-</script>
+            function get(url,receiver) {
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        document.getElementById(receiver).href= this.responseText;
+                        return false;
+                    }
+                };
+                xhr.open ("GET" ,url, true);
+                xhr.send ();
+                return false;
+            }
+            get("https://raw.githubusercontent.com/meisterlab/meisterlab.github.io/master/assets/current.md","meisterlab");
+</script>    
